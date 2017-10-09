169c62df0e00c018152433d1251e979fa0035113
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-10-10 02:48:20 +1100

    test bug 2

---
+++ src/PlayerTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayerUnableToReachLimit() {
+		Player testPlayer = new Player ("noman", 5);
+		int bet = 5;
+		boolean testResult = testPlayer.balanceExceedsLimitBy(bet);
+		assertEquals(false, testResult);
+	}
+	
+	public void testPlayerUnableToReachLimit2() {
+		Player testPlayer = new Player ("noman", 5);
+		int bet = 4;
+		boolean testResult = testPlayer.balanceExceedsLimitBy(bet);
+		assertEquals(true, testResult);
+	}
+
+}
