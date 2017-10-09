f143bac6de20a2cbb49d2292b82bf95a1db58648
nomanahmedkhan <noman-ahmed-khan@live.com>
2017-10-10 04:33:10 +1100

    test bug 5

---
+++ src/DiceValueTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DiceValueTest {
+	
+	@Test
+	public void testSpadesOccurance() {
+		
+		for(int i = 0; i < 100; i++){
+			
+		Dice die = new Dice();
+		System.out.println(die.getValue());
+       
+}
+}
+}
